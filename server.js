2ce3fd15371feef57d3e1a0fe418bfb4576d1d5b
Jonathan Watson <jonathanwatson@Jonathans-MacBook-Pro.local>
2020-08-04 14:21:24 -0400

    updated server.js to serve react build folder

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const app = express();
 
@@ -8,10 +9,16 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static("client/build"));
+
 app.get("/api/config", (req, res) => {
   res.json({
     success: true,
   });
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 mongoose
